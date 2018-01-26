4e883627418622907167b89d7a06860b7b4c3075
Yongwei Hu <yongweihu@users.noreply.github.com>
2018-01-26 12:19:55 +0800

    优化新闻生成流程；支持Scientific American Podcast

--- Classes/MWFeedItem.m
+++ Classes/MWFeedItem.m
@@ -33,7 +33,7 @@
 
 @implementation MWFeedItem
 
-@synthesize identifier, title, link, date, updated, summary, content, author, enclosures;
+@synthesize identifier, title, link, date, updated, summary, content, author, enclosures, categories;
 
 #pragma mark NSObject
 
